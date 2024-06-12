3dceb80bb0ce3dff1dc237dd966d59caad0c107c
KevinTan19 <kevintan.dev96@gmail.com>
2024-06-13 03:46:17 +0700

    add navbar & sidebar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <div className="w-full min-h-[60px] bg-white drop-shadow flex items-center px-4 justify-between">
+      <p className="font-[15px] capitalize text-[#052A49] font-bold">
+        my brand
+      </p>
+      <div>
+        <p>Cooper Rosser</p>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/sidebar.jsx
@@ -0,0 +1,11 @@
+const Sidebar = () => {
+  return (
+    <div className="h-[100vh] bg-[#052A49] max-w-fit py-6">
+      <div className="w-[230px] flex items-center justify-center py-2 px-4 bg-[#084577]">
+        <p className="text-sm text-center capitalize">order management</p>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
